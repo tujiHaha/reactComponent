c66a5cb6d091d261d187845e49990db44c04223d
xin.yue <xin.yue@weimob.com>
2021-03-21 22:07:57 +0800

    sss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { default as Button } from './components/Button'
 import { default as Alert } from './components/Alert'
 import { default as Menu } from './components/Menu'
 import { default as Tab } from './components/Tab'
-
 export {
   Button,
   Alert,
